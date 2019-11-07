eb3e7bc0797b06b1ec52155e5b958cb134e17301
astilphen000 <astilphen000@west-mec.org>
2019-11-06 17:57:03 -0700

    Worked in terminal

---
+++ Docker-Practice/NEM/random.js
@@ -0,0 +1,35 @@
+db.coding.insert({
+	name: 'angelina',
+	Table: 2,
+	hobbies: ['Coding', 'Coloring']
+}) 
+
+db.coding.insert({
+	name: 'Lennye',
+	Table: 2,
+	hobbies: ['Coding', 'Reading', 'Running']
+}) 
+
+db.coding.insert({
+	name: 'Christian',
+	Table: 2,
+	hobbies: ['Coding']
+}) 
+
+db.coding.insert({
+	name: 'Dejah',
+	Table: 2,
+	hobbies: ['Coding']
+}) 
+
+db.coding.insert({
+	name: 'Micah',
+	Table: 2,
+	hobbies: ['Coding']
+}) 
+
+db.coding.insert({
+	name: 'Anthony',
+	Table: 2,
+	hobbies: ['Coding']
+}) 
